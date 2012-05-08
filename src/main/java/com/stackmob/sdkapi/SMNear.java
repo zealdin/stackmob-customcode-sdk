a2f06f61c74842bf0989b35dbd88a9bd62ae351e
Miles O'Connell <miles@stackmob.com>
2012-05-07 17:10:57 -0700

    Add geo query support

---
+++ src/main/java/com/stackmob/sdkapi/SMNear.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2011-2012 StackMob
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.stackmob.sdkapi;
+
+/**
+ * A "near" query. A near query will add the distance from the search center to query results, and will automatically sort query results by their distance from the search center.
+ */
+public class SMNear extends SMCondition {
+  private final String field;
+  private final SMDouble lat;
+  private final SMDouble lon;
+  private final SMDouble dist;
+
+  /**
+   * Create a new SMNear query
+   *
+   * @param field the geoField to query
+   * @param lat the latitude of the location to search
+   * @param lon the longitude of the location to search
+   * @param dist the distance, in radians, to search, used to limit search results
+   */
+  public SMNear(String field, SMDouble lat, SMDouble lon, SMDouble dist) {
+    this.field = field;
+    this.lat = lat;
+    this.lon = lon;
+    this.dist = dist;
+  }
+
+  public String getField() {
+    return field;
+  }
+
+  public SMDouble getLat() {
+    return lat;
+  }
+
+  public SMDouble getLon() {
+    return lon;
+  }
+
+  public SMDouble getDist() {
+    return dist;
+  }
+
+  @Override
+  public String toString() {
+    String withoutDist = String.format("%s near %s, %s", field, lat.toString(), lon.toString());
+    if (dist != null)
+      return withoutDist + String.format(" within %s radians", dist.toString());
+    else
+      return withoutDist;
+  }
+}
---
+++ src/main/java/com/stackmob/sdkapi/SMWithinBox.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright 2011-2012 StackMob
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.stackmob.sdkapi;
+
+/**
+ * A "within" query for searching within a rectangular area
+ */
+public class SMWithinBox extends SMCondition {
+  private final String field;
+  private final SMDouble latLL;
+  private final SMDouble lonLL;
+  private final SMDouble latUR;
+  private final SMDouble lonUR;
+
+  /**
+   * Create a new SMWithinBox query
+   *
+   * @param field the geoField to query
+   * @param latLL the latitude of the lower-left corner of the bounding rectangle
+   * @param lonLL the longitude of the lower-left corner of the bounding rectangle
+   * @param latUR the latitute of the upper-right corner of the bounding rectangle
+   * @param lonUR the longitude of the upper-right corner of the bounding rectangle
+   */
+  public SMWithinBox(String field, SMDouble latLL, SMDouble lonLL, SMDouble latUR, SMDouble lonUR)  {
+    this.field = field;
+    this.latLL = latLL;
+    this.lonLL = lonLL;
+    this.latUR = latUR;
+    this.lonUR = lonUR;
+  }
+
+  public String getField() {
+    return field;
+  }
+
+  public SMDouble getLatLL() {
+    return latLL;
+  }
+
+  public SMDouble getLonLL() {
+    return lonLL;
+  }
+
+  public SMDouble getLatUR() {
+    return latUR;
+  }
+
+  public SMDouble getLonUR() {
+    return lonUR;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s within box between %s, %s and %s, %s", field, latLL.toString(), lonLL.toString(), latUR.toString(), lonUR.toString());
+  }
+}
