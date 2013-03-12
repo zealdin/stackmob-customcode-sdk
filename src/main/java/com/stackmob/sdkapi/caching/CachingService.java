fa9541eb78554f6718b3a5a592ffc7777f1f7ed2
Aaron Schlesinger <arschles@gmail.com>
2013-03-11 19:36:51 -0700

    adding prototype CachingService

---
+++ src/main/java/com/stackmob/sdkapi/caching/CachingService.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2012 StackMob
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.stackmob.sdkapi.caching;
+
+/**
+ * CachingService provides fast, in memory access to data, like the results of DataService queries.
+ * Data is stored as key/value pairs. And since each key/value pair is stored in memory, it could disappear at any time.
+ * Here's a good pattern for how to use CachingService:
+ * <code>
+ *     CachingService cachingService = sdkServiceProvider.
+ * </code>
+ */
+public class CachingService {
+}
